6fafb8cf731044be8f3f81e260b4886fc8b59669
dolodmg <oranotherway@gmail.com>
2025-07-23 00:16:24 -0300

    labels para clases enumerativas

---
+++ src/utils/enumLabels.js
@@ -0,0 +1,36 @@
+export const albumGenreLabels = {
+  CLASSICAL: "Música clásica",
+  FOLKLORE: "Folklore",
+  HEAVY_METAL: "Heavy Metal",
+  HIP_HOP: "Hip Hop",
+  JAZZ: "Jazz",
+  POP: "Pop",
+  ROCK: "Rock",
+};
+
+export const albumFormatLabels = {
+    CD: "CD",
+    VINYL: "Vinilo"
+};
+
+export const bookGenreLabels = {
+    BIOGRAPHY: "Biografía",
+    CHILDREN: "Niños",
+    FANTASY: "Fantasía",
+    GRAPHIC_FICTION: "Ficción gráfica",
+    HISTORICAL_FICTION: "Ficción histórica",
+    HISTORY: "Historia",
+    HORROR: "Horror",
+    HUMANITIES: "Humanidades",
+    HUMOR: "Humor",
+    LITERARY_FICTION: "Ficcón literaria",
+    MYSTERY: "Misterio",
+    RELIGION: "Religión",
+    ROMANCE: "Romance",
+    SCIENCE_FICTION: "Ciencia ficción",
+    SCIENCE_TECHNOLOGY: "Ciencia y tecnología",
+    SELF_HELP: "Autoayuda",
+    SHORT_STORY: "Historias cortas",
+    THRILLER: "Thriller",
+    YOUNG_ADULT: "Jóvenes adultos"
+};
